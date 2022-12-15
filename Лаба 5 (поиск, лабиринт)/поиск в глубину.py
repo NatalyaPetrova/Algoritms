fda18624bc8c5d67d921a650fe97b5bcae4becc7
NatalyaPetrova <112980031+NatalyaPetrova@users.noreply.github.com>
2022-12-15 22:27:15 +0300

    Add files via upload

---
+++ Лаба 5 (поиск, лабиринт)/поиск в глубину.py
@@ -0,0 +1,40 @@
+import timeit
+
+graph = {1: set([2, 3, 4]),
+         2: set([5, 6]),
+         3: set([]),
+         4: set([7, 8]),
+         5: set([9, 10]),
+         6: set([]),
+         7: set([11, 12]),
+         8: set([]),
+         9: set([]),
+         10: set([]),
+         11: set([]),
+         12: set([]),
+         }
+
+
+def dfs_paths(graph, start, goal):
+    stack = [(start, [start])]
+    while stack:
+        (vertex, path) = stack.pop()
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                stack.append((next, path + [next]))
+
+
+def shortest_path(graph, start, goal):
+    try:
+        return next(dfs_paths(graph, start, goal))
+    except StopIteration:
+        return None
+
+
+start_time = timeit.default_timer()
+print('Кратчайший путь', shortest_path(graph, int(input('Введите начало:')), int(input('Введите конец:'))))
+end_time = timeit.default_timer() - start_time
+
+print('Время выполнения поиска кратчайшего пути (DFS)', end_time)
